4bd8373b0c0a67cd646ad169a8fbee9547c1ad7e
Reza Taheri <taheri.reza.01@gmail.com>
2024-09-08 14:18:39 +0330

    Update leetcode_0003.py

--- leetcode_0003.py
+++ leetcode_0003.py
@@ -1,17 +1,26 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        charDict = {}
+        char_dict = {}
         max_len = 0
-        start = 0
+        start = 0  # Start of the sliding window
 
-        for end in range(len(s)):
+        for end in range(len(s)): # O(n)
             char = s[end]
 
-            if char in charDict and charDict[char] >= start:
-                charDict[char] + 1
+            if char in char_dict and char_dict[char] >= start:
+                # Move start pointer to the right of the previous occurrence of char
+                start = char_dict[char] + 1
 
-            charDict[char] = end
+            # Update the latest index of the character
+            char_dict[char] = end
 
+            # Calculate the max length of the substring
             max_len = max(max_len, end - start + 1)
 
         return max_len
+
+
+s = Solution()
+print(s.lengthOfLongestSubstring("abcabcbb"))
+print(s.lengthOfLongestSubstring("bbbbb"))
+print(s.lengthOfLongestSubstring("pwwkew"))
