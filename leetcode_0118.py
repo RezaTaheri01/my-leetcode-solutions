4cb901e98a5c2deb087721a3710fa2efc17d8337
Reza Taheri <taheri.reza.01@gmail.com>
2024-10-13 22:33:04 +0330

    Create leetcode_0118.py

---
+++ leetcode_0118.py
@@ -0,0 +1,36 @@
+# Todo: O(n) time complexity
+
+class Solution: # O(n^2) 
+    def generate(self, numRows: int) -> list[list[int]]:
+        pascal: list = [[1], [1, 1]]  # init
+        row_len = 2  # current level(deepness) that also current size
+
+        # Edge cases
+        if numRows < 3:
+            return pascal[:numRows]
+
+        # Main Process
+        temp = []
+        while row_len < numRows:
+            row_len += 1
+
+            for i in range(row_len):
+                if i == 0 or i == row_len - 1:
+                    temp.append(1)
+                else:
+                    temp.append(pascal[row_len-2][i-1] + pascal[row_len-2][i])
+
+            pascal.append(temp)
+            temp = []
+
+        return pascal
+
+
+def main():
+    s = Solution()
+    result = s.generate(5)
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
