13b5f3f76d71c06462cdb30a584e29bdfd6978b4
Reza Taheri <taheri.reza.01@gmail.com>
2024-11-07 16:41:04 +0330

    Update leetcode_0389.py

--- leetcode_0389.py
+++ leetcode_0389.py
@@ -3,10 +3,7 @@ class Solution:
         counts: dict = {}
         # count characters
         for char in s:
-            if char in counts:
-                counts[char] += 1
-            else:
-                counts[char] = 1
+            counts[char] = counts.get(char, 0) + 1
 
         for char in t:
             if char in counts and counts[char] > 0:
