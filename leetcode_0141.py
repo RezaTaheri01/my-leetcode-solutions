da459228fb3bb425e1031cfc15adefcd5f4a000a
Reza Taheri <taheri.reza.01@gmail.com>
2024-11-10 01:23:59 +0330

    Create leetcode_0141.py

---
+++ leetcode_0141.py
@@ -0,0 +1,18 @@
+from typing import Optional
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        if not head:
+            return False
+        if head.val == "-":
+            return True
+        head.val = "-"
+        return self.hasCycle(head.next)
