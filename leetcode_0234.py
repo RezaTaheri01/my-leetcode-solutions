50db4c06fb85f5a9dbd7ed038ade1d80ed4af9bf
Reza Taheri <taheri.reza.01@gmail.com>
2024-11-14 23:10:02 +0330

    Create leetcode_0234.py

---
+++ leetcode_0234.py
@@ -0,0 +1,20 @@
+from typing import Optional
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        li: list = []
+
+        # navigate on linked list
+        while head:
+            li.append(head.val)
+            head = head.next
+
+        return li == li[::-1]
