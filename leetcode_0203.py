6620dd38d36b0749ff39b0cc84ce546c5145c5d1
Reza Taheri <taheri.reza.01@gmail.com>
2024-11-14 23:21:53 +0330

    Create leetcode_0203.py

---
+++ leetcode_0203.py
@@ -0,0 +1,28 @@
+from typing import Optional
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
+        prev = None
+        curr = head
+
+        # navigate on linked list
+        while curr:
+            if curr.val == val:
+                if prev:
+                    prev.next = curr.next
+                else:
+                    head = curr.next
+            else:
+                prev = curr
+
+            curr = curr.next
+
+        return head
