93371e105a73e6a5728c8b39fc9019b6386ad80a
Reza Taheri <94985322+RezaTaheri01@users.noreply.github.com>
2025-04-12 20:55:13 +0330

    Create leetcode_1323.py

---
+++ leetcode_1323.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maximum69Number(self, num: int) -> int:
+        return int(str(num).replace("6", "9", 1))
+
+
+s = Solution()
+
+print(s.maximum69Number(9669))
+print(s.maximum69Number(9999))
