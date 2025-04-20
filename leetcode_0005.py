305c7dde6a89e7fedcbf573bdbc34df2ff1e8930
Reza Taheri <94985322+RezaTaheri01@users.noreply.github.com>
2025-04-20 22:38:48 +0330

    Update leetcode_0005.py

--- leetcode_0005.py
+++ leetcode_0005.py
@@ -1,28 +1,33 @@
-# Todo: Improve time complexity
-
 class Solution:
+    def __init__(self):
+        self.longest = ""
+        self.longest_len = 0
+
     def longestPalindrome(self, s: str) -> str:
-        def isPalindrome(start, end) -> bool:
-            pl, pr = start, end
-
-            while pl < pr:
-                if s[pl] != s[pr]:
-                    return False
-                pl += 1
-                pr -= 1
-            return True
-
         l = len(s)
-        for i in range(l-1, -1, -1):
-            start = 0
-            while start + i < l:
-                if isPalindrome(start, start + i):
-                    return s[start: start + i + 1]
-                start += 1
+        self.longest = ""
+        self.longest_len = 0
 
+        def _longestPalindrome(pl: int, pr: int) -> str:
+            while pl >= 0 and pr < l and s[pl] == s[pr]:
+                if pr - pl + 1 > self.longest_len:
+                    self.longest = s[pl:pr + 1]
+                    self.longest_len = pr - pl + 1
+
+                pl -= 1
+                pr += 1
+
+        for i in range(l):
+            # odd
+            _longestPalindrome(i, i)
+            # even
+            _longestPalindrome(i, i + 1)
+
+        return self.longest
 
 
 s = Solution()
 
-print(s.longestPalindrome(s = "babad"))
-print(s.longestPalindrome(s = "cbbd"))
+print(s.longestPalindrome(s="babad"))
+print(s.longestPalindrome(s="cbbd"))
+print(s.longestPalindrome(s="c"))
