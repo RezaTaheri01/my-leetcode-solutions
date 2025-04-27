715e0281ee3dba30761f5e9e43d31372e65247c2
Reza Taheri <94985322+RezaTaheri01@users.noreply.github.com>
2025-04-27 15:54:58 +0330

    Update leetcode_0322.py

--- leetcode_0322.py
+++ leetcode_0322.py
@@ -6,18 +6,21 @@
 # Base Case: dp[0] = 0 (0 coins are needed for amount 0).
 class Solution:
     def coinChange(self, coins: list[int], amount: int) -> int:
-        dp = [amount + 1] * (amount + 1)  # Initialize DP array with "infinity"
-        dp[0] = 0  # Base case: 0 coins needed for amount 0
-
-        for coin in coins:  # Process each coin
-            for j in range(coin, amount + 1):  # Update DP array
-                dp[j] = min(dp[j], dp[j - coin] + 1)
-
+        dp = [amount + 1] * (amount + 1) 
+        dp[0] = 0
+        
+        for a in range(1, amount + 1):
+            for coin in coins:
+                if coin > a:
+                    break
+                dp[a] = min(dp[a], 1 + dp[a - coin])
+    
         return dp[amount] if dp[amount] != amount + 1 else -1
+    
 
 
 
 
 s = Solution()
-print(s.coinChange([2, 5], 11))
+print(s.coinChange([1, 2, 5], 11))
 print(s.coinChange([186,419,83,408], 6249))
